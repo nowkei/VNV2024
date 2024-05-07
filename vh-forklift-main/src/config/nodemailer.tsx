53e83c135b190b0b88bfe7f3a5553a7620fcfcf2
nguyen.tuananh <nguyen.tuananh@petronas.com>
2024-05-07 23:18:23 +0700

    second commit

--- vh-forklift-main/src/config/nodemailer.tsx
+++ vh-forklift-main/src/config/nodemailer.tsx
@@ -13,5 +13,5 @@ export const transporter = nodemailer.createTransport({
 
 export const mailOptions = {
   from: email,
-  to: 'hophanamokee@gmail.com',
+  to: 'congnghegiaoducvinguoiviet@gmail.com',
 };

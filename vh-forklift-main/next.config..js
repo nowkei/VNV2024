90f8d5dffb8fe92f08a2592e67e4aa08511fa3ed
nguyen.tuananh <nguyen.tuananh@petronas.com>
2024-05-07 23:24:23 +0700

    commit deploy

--- vh-forklift-main/next.config..js
+++ vh-forklift-main/next.config..js
@@ -14,4 +14,9 @@ module.exports = withBundleAnalyzer({
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized : true
+  }
 });
